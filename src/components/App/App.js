5f54e8cb78b523443e2b8c7f9144795574918784
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-20 16:16:07 +0300

    feat: подключить провайдеры в проект

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './App.css';
 import Router from './Router/Router';
 import Popup from '../ui/Popup/Popup';
+import AuthProvider from '../../provider/AuthProvider';
+import PopupProvider from '../../provider/PopupProvider';
+import CurrentUserProvider from '../../provider/CurrentUserProvider';
 
 const App = () => {
-  const [isPopupOpen, setIsPopupOpen] = React.useState(false);
 
   return (
-    <div className='App'>
-      <Popup isOpen={isPopupOpen} setIsOpen={setIsPopupOpen} />
-      <Router />
-    </div>
+    <AuthProvider>
+      <CurrentUserProvider>
+        <PopupProvider>
+          <div className='App'>
+            <Popup />
+            <Router />
+          </div>
+        </PopupProvider>
+      </CurrentUserProvider>
+    </AuthProvider>
   );
 };
 

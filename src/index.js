b01e593b0f8239cec86e2c5ed892b2eaff329a8f
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-21 13:54:39 +0300

    refactor: перенести подключение провайдеров из App в index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './provider/AuthProvider';
+import CurrentUserProvider from './provider/CurrentUserProvider';
+import PopupProvider from './provider/PopupProvider';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <CurrentUserProvider>
+          <PopupProvider>
+            <App />
+          </PopupProvider>
+        </CurrentUserProvider>
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );

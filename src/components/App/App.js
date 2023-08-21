251b4b0b3dc17341510e88e75eb6f07a7f1522a5
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-21 13:57:56 +0300

    feat: добавить проверку токена и получение информации о пользователе в App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './App.css';
 import Router from './Router/Router';
 import Popup from '../ui/Popup/Popup';
-import AuthProvider from '../../provider/AuthProvider';
-import PopupProvider from '../../provider/PopupProvider';
-import CurrentUserProvider from '../../provider/CurrentUserProvider';
+import AuthContext from '../../context/AuthContext';
+import { checkAuth } from '../../services/auth.service';
+import MainApi from '../../utils/MainApi';
+import CurrentUserContext from '../../context/CurrentUserContext';
+import Preloader from '../ui/Preloader/Preloader';
 
 const App = () => {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+  const { setUser } = useContext(CurrentUserContext);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    checkAuth()
+      .then(res => {
+        if (res)
+          setIsAuth(true);
+        else
+          setIsAuth(false);
+      })
+      .finally(() => setIsLoading(false));
+  }, [setIsAuth]);
+
+  useEffect(() => {
+    MainApi.getUser()
+      .then(user => {
+        if (user) setUser(user);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [isAuth, setUser]);
+
+  useEffect(() => {
+    console.log('App render');
+
+    // return () => {
+    //   console.log('App unrender');
+    // }
+  }, []);
 
   return (
-    <AuthProvider>
-      <CurrentUserProvider>
-        <PopupProvider>
-          <div className='App'>
-            <Popup />
-            <Router />
-          </div>
-        </PopupProvider>
-      </CurrentUserProvider>
-    </AuthProvider>
+    <div className='App'>
+      <Popup />
+      {/*<Router />*/}
+      {isLoading ?
+        <Preloader />
+        :
+        <Router />
+      }
+    </div>
   );
 };
 

4459d37bd8d6ea287c23bd02adc747206144f4e3
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-22 21:26:27 +0300

    feat: сделать валидацию формы на странице авторизации

--- src/components/pages/LoginPage/LoginPage.jsx
+++ src/components/pages/LoginPage/LoginPage.jsx
@@ -1,46 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './LoginPage.css';
 import Header from '../../modules/Header/Header';
-import Form from '../../modules/Form/Form';
-import InputGroup from '../../ui/form/InputGroup/InputGroup';
-import SubmitButton from '../../ui/form/SubmitButton/SubmitButton';
-import { Link, useNavigate } from 'react-router-dom';
-import { login } from '../../../services/auth.service';
-import PopupContext from '../../../context/PopupContext';
-import AuthContext from '../../../context/AuthContext';
+import { Link } from 'react-router-dom';
+import LoginForm from './LoginForm/LoginForm';
 
 const LoginPage = () => {
-  const Popup = useContext(PopupContext);
-  const { setIsAuth } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleFormSubmit = event => {
-    event.preventDefault();
-    login('user3@test.ru', 'test', Popup, setIsAuth)
-      .then(() => {
-        navigate('/movies', { replace: true });
-      });
-  };
-
   return (
     <div className='login'>
       <Header classes='login__header' />
       <main className='main'>
         <section className='login__form-section'>
           <h1 className='login__title'>Рады видеть!</h1>
-          <Form handleSubmit={handleFormSubmit}>
-            <InputGroup titleText='E-mail' inputType='email' inputId='input-email' />
-            <InputGroup
-              titleText='Пароль'
-              inputType='password'
-              inputId='input-password'
-              validators={{
-                minLength: 6,
-                maxLength: 12
-              }}
-            />
-            <SubmitButton classes='login__submit-button' text='Войти' />
-          </Form>
+          <LoginForm />
         </section>
         <section className='login__navigation'>
           <p className='login__text'>Ещё не зарегистрированы?</p>

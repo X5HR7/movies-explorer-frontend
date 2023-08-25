f9c4e956aad51ede05f4366e6f92ed032f267542
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-25 13:03:51 +0300

    refactor: подключить все провайдеры в Provider.jsx, а его в index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from './provider/AuthProvider';
-import CurrentUserProvider from './provider/CurrentUserProvider';
-import PopupProvider from './provider/PopupProvider';
+import Provider from './provider/Provider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CurrentUserProvider>
-          <PopupProvider>
-            <App />
-          </PopupProvider>
-        </CurrentUserProvider>
-      </AuthProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>
   // </React.StrictMode>
 );

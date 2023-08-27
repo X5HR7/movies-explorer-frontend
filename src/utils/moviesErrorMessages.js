ca9980fa91908ed7073640c1d78dd15921fef5e4
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2023-08-27 14:25:52 +0300

    feat: вынести сообщения об ошибках при фильтрации и получении фильмов в файл moviesErrorMessages.js

---
+++ src/utils/moviesErrorMessages.js
@@ -0,0 +1,8 @@
+const errorMessages = {
+  serverError:
+    'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз',
+  validationError: 'Нужно ввести ключевое слово',
+  notFoundError: 'Ничего не найдено'
+};
+
+export default errorMessages;
